986c7c0fa8ee162f97e4a4c3ad080df306a44405
the-hof <genghisuber@gmail.com>
2014-03-20 15:03:34 -0400

    added table results, changed to new recommenditem API call

--- index-controller.js
+++ index-controller.js
@@ -6,27 +6,53 @@ rpmApp.controller('IndexCtrl', function ($scope, $http) {
   $scope.itemid = '7';
   $scope.numitems = '25';
 
+  $scope.item_description = '';
+
+  $scope.lookupItem = function (itemid) {
+    var api_url = 'http://127.0.0.1:5000/getItem?callback=JSON_CALLBACK&item_id=';
+
+    if (itemid) {
+      if (itemid.charAt(0) != 'I')
+        api_url += 'I';
+      api_url += itemid;
+      $http.jsonp(api_url).success(function (data) {
+        $scope.item_description = data[0].name;
+      });
+    }
+  }
+
   $scope.getRecommendations = function (userid, itemid, numitems) {
-    var api_url = 'http://127.0.0.1:5000/recommender/'
+    var api_url = 'http://127.0.0.1:5000/recommenderitems/'
     api_url += userid + '/';
     api_url += itemid + '/';
     api_url += numitems
     api_url += '?callback=JSON_CALLBACK';
 
-    $http.jsonp(api_url).success(getRecommendationsCallback);
-
-    function getRecommendationsCallback(data) {
+    $http.jsonp(api_url).success(function (data) {
       var api_results = data;
       $scope.results = [];
+      var recommendationResults = [];
 
+      $scope.results = api_results;
+      console.log(data);
+      /*
       for (var key in api_results) {
         if (api_results.hasOwnProperty(key)) {
-          $scope.results.push({
+          var recommendationItem = {
             'itemid': key,
-            'score' : api_results[key]
-          })
+            'score' : api_results[key],
+            'item_description': ''
+          }
+          recommendationResults.push(recommendationItem);
         }
       }
-    }
+
+      recommendationResults.sort(function(a, b) {
+        return b.score - a.score;
+      });
+
+      $scope.results = recommendationResults;
+      */
+    });
   }
 });
